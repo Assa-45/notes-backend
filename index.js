21af5262c535068cd12c4abc0663374b32b570c2
Assa <salmaassa975@gmail.com>
2026-05-11 22:17:16 +0700

    Update server listen address for external access
    Change server listen address to '0.0.0.0' for external access.

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ app.use("/api/v1/notes", noteRoutes);
 const port = process.env.PORT || 3000;
 sequelize.sync().then(() => {
     console.log("Database ready");
-    app.listen(port, () => console.log(`Server running on port ${port}`));
-});
-
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed:");
+    console.error(err);
+  });
